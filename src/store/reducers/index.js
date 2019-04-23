b4e8373afa5a0bd09b3fec424e830b2c9776eb92
rheeger <rheeger@gmail.com>
2019-04-23 10:28:02 -0700

    getting uniswap reducers and abis integrated plus prettier for givingTree contracts

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,11 @@
 import { combineReducers } from 'redux';
 import { orgs, org } from './orgs';
+import swapAddresses from './swapAddresses';
+import web3connect from './web3connect';
 
 export default combineReducers({
 	orgs: orgs,
-	org: org
+	org: org,
+	addresses: swapAddresses,
+	web3connect: web3connect
 });
