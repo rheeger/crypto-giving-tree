8dc5a933e1b5424e88ba6451175b1d31f4693bb4
rheeger <rheeger@gmail.com>
2019-04-24 10:55:41 -0700

    got the web3 login popup wired. onwards

--- pages/orgs/donate.js
+++ pages/orgs/donate.js
@@ -1,10 +1,48 @@
 import React from 'react';
-import ContributionForm from '../../src/components/ContributionForm';
+import { connect } from 'react-redux';
+// import ContributionForm from '../../src/components/ContributionForm';
+import { Web3Connect, startWatching, initialize } from '../../src/store/reducers/web3connect';
+import { setAddresses } from '../../src/store/reducers/swapAddresses';
 
 class Donate extends React.Component {
+	componentWillMount() {
+		const { initialize, startWatching } = this.props;
+		initialize().then(startWatching);
+	}
+
+	componentWillUpdate() {
+		const { web3, setAddresses } = this.props;
+
+		if (this.hasSetNetworkId || !web3 || !web3.eth || !web3.eth.net || !web3.eth.net.getId) {
+			return;
+		}
+
+		web3.eth.net.getId((err, networkId) => {
+			if (!err && !this.hasSetNetworkId) {
+				setAddresses(networkId);
+				this.hasSetNetworkId = true;
+			}
+		});
+	}
+
 	render() {
-		return <ContributionForm />;
+		if (!this.props.initialized) {
+			return <noscript />;
+		}
+
+		return <Web3Connect />;
 	}
 }
 
-export default Donate;
+export default connect(
+	(state) => ({
+		account: state.web3connect.account,
+		initialized: state.web3connect.initialized,
+		web3: state.web3connect.web3
+	}),
+	(dispatch) => ({
+		setAddresses: (networkId) => dispatch(setAddresses(networkId)),
+		initialize: () => dispatch(initialize()),
+		startWatching: () => dispatch(startWatching())
+	})
+)(Donate);
