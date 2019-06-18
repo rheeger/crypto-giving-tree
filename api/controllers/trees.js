5e56658821a358d93fa01625f434fb09674a024a
rheeger <rheeger@gmail.com>
2019-06-18 09:59:16 -0700

    starting to have something set up here for mongo

--- api/controllers/trees.js
+++ api/controllers/trees.js
@@ -1,3 +1,53 @@
+const Tree = require('./models/trees');
+
 exports.allTrees = function(req, res, next) {
-	res.send({ success: true });
+	// use mongoose to get all trees in the database
+	Tree.find(function(err, trees) {
+		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+		if (err) res.send(err);
+
+		res.json(trees); // return all trees in JSON format
+	});
+};
+
+exports.oneTree = function(req, res, next) {
+	// use mongoose to get one tree in the database
+	Tree.findById(req.params.id, function(err, tree) {
+		// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+		if (err) res.send(err);
+
+		res.json(tree); // return all trees in JSON format
+	});
+};
+
+exports.createTree = function(req, res, next) {
+	// use mongoose to create one tree in the database
+	Tree.create(
+		{
+			id: req.body.id,
+			branchName: req.body.branchName,
+			primaryAdvisorFirstName: req.body.primaryAdvisorFirstName,
+			primaryAdvisorLastName: req.body.primaryAdvisorLastName,
+			primaryAdvisorEmail: req.body.primaryAdvisorEmail,
+			primaryAdvisorAddress: req.body.primaryAdvisorAddress,
+			primaryAdvisorCity: req.body.primaryAdvisorCity,
+			primaryAdvisorState: req.body.primaryAdvisorState,
+			primaryAdvisorZip: req.body.primaryAdvisorZip,
+			primaryAdvisorBirthday: req.body.primaryAdvisorBirthday,
+			managerAddress: req.body.managerAddress,
+			treeDAI: req.body.treeDAI,
+			datePlanted: req.body.datePlanted,
+			grantableDAI: req.body.grantableDAI
+		},
+		function(err, todo) {
+			if (err) res.send(err);
+			// use mongoose to get one tree in the database
+			Tree.findById(req.body.id, function(err, tree) {
+				// if there is an error retrieving, send the error. nothing after res.send(err) will execute
+				if (err) res.send(err);
+
+				res.json(tree); // return all trees in JSON format
+			});
+		}
+	);
 };
--- api/router.js
+++ api/router.js
@@ -5,5 +5,34 @@ const Orgs = require('./controllers/orgs');
 const Trees = require('./controllers/trees');
 
 module.exports = function(app) {
+	//Trees
 	app.get('/trees', Trees.allTrees);
+	app.get('/trees/:id', Trees.oneTree);
+	app.post('/trees', Trees.createTree);
+	app.patch('/trees/:id', Trees.updateTree);
+	app.delete('/trees/:id', Trees.deleteTree);
+	//Orgs
+	app.get('/orgs', Orgs.allOrgs);
+	app.get('/orgs/:id', Orgs.oneOrg);
+	app.post('/orgs', Orgs.createOrg);
+	app.patch('/orgs/:id', Orgs.updateOrg);
+	app.delete('/orgs/:id', Orgs.deleteOrg);
+	//Grants
+	app.get('/grants', Grants.allGrants);
+	app.get('/grants/:id', Grants.oneGrant);
+	app.post('/grants', Grants.createGrant);
+	app.patch('/grants/:id', Grants.updateGrant);
+	app.delete('/grants/:id', Grants.deleteGrant);
+	//Donations
+	app.get('/donations', Donations.allDonations);
+	app.get('/donations/:id', Donations.oneDonation);
+	app.post('/donations', Donations.createDonation);
+	app.patch('/donations/:id', Donations.updateDonation);
+	app.delete('/donations/:id', Donations.deleteDonation);
+	//Claims
+	app.get('/claims', Claims.allClaims);
+	app.get('/claims/:id', Claims.oneClaim);
+	app.post('/claims', Claims.createClaim);
+	app.patch('/claims/:id', Claims.updateClaim);
+	app.delete('/claims/:id', Claims.deleteClaim);
 };
