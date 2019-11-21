9af70e8f069c8a2e96741a7a1884bc43173d2f77
rheeger <rheeger@gmail.com>
2019-11-20 18:17:16 -0800

    https-ifying the server

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const https = require('https');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
@@ -14,6 +15,13 @@ app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
+const cert = fs.readFileSync('./cert.pem');
+const key = fs.readFileSync('./key.pem');
+const pass = 'CharityBlock';
+const credentials = { key: key, cert: cert, pass };
+
 const server = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 server.listen(80, '10.128.0.3');
 console.log('Server listening on port 80');
+httpsServer.listen(8443, '10.128.0.3');
