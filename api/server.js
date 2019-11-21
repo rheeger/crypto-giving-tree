6b778a2d73e50806a5b4447bb42b3739674a4526
rheeger <rheeger@gmail.com>
2019-11-21 15:34:02 -0800

    WEVE FIXED OUR ENDPOINT :

--- api/server.js
+++ api/server.js
@@ -1,30 +1,30 @@
 const express = require('express');
 const http = require('http');
-const https = require('https');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const app = express();
 const router = require('./router');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const fs = require('fs');
+// const https = require('https');
+// const fs = require('fs');
 
-mongoose.connect('mongodb://10.128.0.4:80/forest', { useNewUrlParser: true });
+mongoose.connect('mongodb://34.66.15.192:80/forest', { useNewUrlParser: true });
 
 app.use(morgan('combined'));
 app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
-const cert = fs.readFileSync('./cert.pem');
-const key = fs.readFileSync('./key.pem');
-const credentials = { key: key, cert: cert };
-
 const server = http.createServer(app);
-server.listen(80, '10.128.0.3');
+server.listen(8080);
 console.log('Server listening on port 80');
 
-console.log('Attempting launch of https server...');
-const httpsServer = https.createServer(credentials, app);
-httpsServer.listen(443, '10.128.0.3');
-console.log('Secure server listening on port 8443');
+// const cert = fs.readFileSync('./cert.pem');
+// const key = fs.readFileSync('./key.pem');
+// const credentials = { key: key, cert: cert };
+
+// console.log('Attempting launch of https server...');
+// const httpsServer = https.createServer(credentials, app);
+// httpsServer.listen(443, '10.128.0.3');
+// console.log('Secure server listening on port 8443');
--- client/src/apis/localDB.js
+++ client/src/apis/localDB.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'http://34.68.117.220/',
+	baseURL: 'http://api.charityblock.org',
 	rejectUnauthorized: false
 });
